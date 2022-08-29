cc7fc1e344c53a1f1e8bf22524087041d73344b6
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-08-29 16:34:33 -0300

    initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: [
+      'light',
+      {
+        dark: {
+          ...require('daisyui/src/colors/themes')["[data-theme=business]"],
+          primary: '#48b16d',
+          secondary: '#555'
+        }
+      }
+    ]
+  }
+};
