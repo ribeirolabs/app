355414c23f90ff24fe5839f11684e1030cdc34ce
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-08-30 13:26:43 -0300

    fix(nextauth)

--- src/pages/auth/nextauth.ts
+++ src/pages/auth/nextauth.ts
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/server/db/client";
-import { env } from "@/server/env.mjs";
+import { env } from "@/env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
   pages: {
