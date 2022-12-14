f638cd3e947676c5fee1ff374c9ddae3fb8ac40d
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-12-14 20:04:37 -0300

    fix(trpc): always use a base url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,7 @@ MyApp.getInitialProps = async (appContext: AppContext) => {
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== undefined) return ""; // browser should use relative url
+  // if (typeof window !== undefined) return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
