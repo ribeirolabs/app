6127de6321284288a607820c3097c466bd0e7c34
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-12-19 17:47:48 -0300

    feat(ui)

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -8,7 +8,7 @@ export const AppShell = ({ children }: PropsWithChildren) => {
       <AppHeader />
 
       <main className="w-content lg:grid lg:grid-cols-page gap-6 flex-1">
-        <div className="grid-span-1 p-4 md:px-0 print:h-full overflow-hidden">
+        <div className="grid-span-1 py-4 print:h-full overflow-hidden">
           {children}
         </div>
         <Sidebar />
