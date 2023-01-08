8680d72e1877281c021748daedd47fa6a6985769
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2023-01-08 00:03:11 -0300

    feat(theme): add "ping-slow" animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,26 @@ const { theme } = require("../src/app.config.js");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./app-common/src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app-common/src/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   theme: {
     extend: {
       gridTemplateColumns: {
-        'page': 'auto max-content'
+        page: "auto max-content",
       },
       fontSize: {
-        base: '.9rem'
+        base: ".9rem",
+      },
+      animation: {
+        "ping-slow": "ping 2s cubic-bezier(0,0,.2,1) infinite",
       },
     },
     fontFamily: {
-      brand: ['"JetBrains Mono", monospace']
-    }
+      brand: ['"JetBrains Mono", monospace'],
+    },
   },
   daisyui: {
     darkTheme: "business",
@@ -30,7 +36,7 @@ module.exports = {
         },
         dark: {
           ...require("daisyui/src/colors/themes")["[data-theme=business]"],
-          ...theme.colors
+          ...theme.colors,
         },
       },
     ],
